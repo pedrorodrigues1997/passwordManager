0452405b17234f2a05a96bd851e24b2169f46869
Pedro <pedrex1997@gmail.com>
2022-04-05 00:36:51 +0100

    initialCommit

---
+++ src/main/java/launcher/PwManagerLauncher.java
@@ -0,0 +1,14 @@
+package launcher;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class PwManagerLauncher {
+
+    private static final Logger LOGGER = LogManager.getLogger(PwManagerLauncher.class);
+
+    public static void main(String[] args){
+        LOGGER.debug("Hello world");
+    }
+
+}
